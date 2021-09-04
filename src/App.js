242e3231ff71e2b5efad0d84515337aa51945341
LF S <lfs@lfdemacbook-pro.myfiosgateway.com>
2021-09-03 22:16:25 -0400

    integrate app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-import { Layout } from "antd";
+import { Layout, Typography } from "antd";
+import { useState } from "react";
+import FoodList from "./components/FoodList";
+import LoginForm from "./components/LoginForm";
+import MyCart from "./components/MyCart";
 
 const { Header, Content } = Layout;
+const { Title } = Typography;
 
 function App() {
+  const [authed, setAuthed] = useState(false);
+
   return (
     <Layout style={{ height: "100vh" }}>
-      <Header>header</Header>
+      <Header>
+        <div className="header" style={{display: "flex", justifyContent: "space-between"}}>
+          <Title
+            level={2}
+            style={{ color: "white", lineHeight: "inherit", marginBottom: 0 }}
+          >
+            Lai Food
+          </Title>
+          {authed && (
+            <div>
+              <MyCart />
+            </div>
+          )}
+        </div>
+      </Header>
       <Content
         style={{
           padding: "50px",
@@ -13,7 +34,11 @@ function App() {
           overflowY: "auto",
         }}
       >
-        content
+        {authed ? (
+          <FoodList />
+        ) : (
+          <LoginForm onSuccess={() => setAuthed(true)} />
+        )}
       </Content>
     </Layout>
   );
